8e7340c1763d2176341c1bfd47328c18a084310d
Shun <s.tsuchiya.business@gmail.com>
2024-03-25 02:30:44 +0900

    remove redundant file

--- src/presentation.rs
+++ src/presentation.rs
@@ -1,3 +1,2 @@
 pub mod pretty_json_presenter;
 pub mod send_message;
-pub mod show_json;
--- src/presentation/pretty_json_presenter.rs
+++ src/presentation/pretty_json_presenter.rs
@@ -1,7 +1,14 @@
 use crate::domain::proposal::proposal_model::ProposalModel;
-use crate::presentation::show_json::show_json;
+use crate::presentation::send_message::{send_message_to_console, RunningStatus};
 
 pub fn print_pretty_json(proposal: &ProposalModel) {
-    let pretty_json = show_json(proposal).unwrap();
-    println!("{}", pretty_json);
+    let pretty_json = serde_json::to_string_pretty(proposal);
+    match pretty_json {
+        Ok(json) => {
+            send_message_to_console(RunningStatus::Success, &json);
+        }
+        Err(e) => {
+            send_message_to_console(RunningStatus::Failed, &format!("Error: {}", e));
+        }
+    }
 }
